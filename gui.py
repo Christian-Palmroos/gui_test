d96fddd1e4efe4211e074dc8dbe7c29a82b4cdc4
Christian-Palmroos <chospa@utu.fi>
2024-09-26 18:16:33 +0300

    First commit. Imports and main.

---
+++ gui.py
@@ -0,0 +1,17 @@
+
+import os
+import tkinter
+import numpy as np
+
+
+def main():
+    """
+    The main function, executed when running as a script.
+    """
+
+    print(":D")
+
+
+if __name__ == "__main__":
+
+    main()
