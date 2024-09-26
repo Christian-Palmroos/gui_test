ebcc1e832e481962252587f354b322678764478b
Christian-Palmroos <chospa@utu.fi>
2024-09-26 18:34:59 +0300

    commit

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
 #!/home/chospa/anaconda3/bin/python
 
 import os
-import tkinter
+import tkinter as tk
+from tkinter import ttk
 import numpy as np
 
 
@@ -10,7 +11,15 @@ def main():
     The main function, executed when running as a script.
     """
 
-    print(":D")
+    main_window = tk.Tk()
+
+    frame = ttk.Frame(main_window, padding=100)
+    frame.grid()
+
+    ttk.Label(frame, text="Huutista :D").grid(column=0, row=0)
+    ttk.Button(frame, text="Lopeta D:", command=main_window.destroy).grid(column=1, row=0)
+
+    main_window.mainloop()
 
 
 if __name__ == "__main__":
